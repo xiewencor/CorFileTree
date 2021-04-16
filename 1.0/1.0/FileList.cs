e8b1863266e9b08401409705c2fc68a8722ff85f
xiewencor <xiewencor@163.com>
2021-04-17 05:12:50 +0800

    Add files via upload

---
+++ 1.0/1.0/FileList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Cor
+{
+    class FileList
+    {
+        StreamWriter sw;
+        public  void Start(string dir , string file)
+        {
+            sw = new StreamWriter(file, false, Encoding.Default);
+            sw.WriteLine(dir);
+            WriteFileList(dir);
+            sw.Flush();
+            sw.Close();
+        }
+        private void WriteFileList(string dir)
+        {
+            string[] dirs;
+            try
+            {
+                dirs = Directory.GetDirectories(dir);
+            }
+            catch (Exception)
+            {
+                System.Windows.Forms.MessageBox.Show("出现错误,目录损坏或无权访问!\n" + dir, "错误警告");
+                throw;
+            }
+            string[] files = Directory.GetFiles(dir);
+            for (int i = 0; i < dirs.Length; i++)
+            {
+                sw.WriteLine(dirs[i]);
+                WriteFileList(dirs[i]);
+
+            }
+            for (int i = 0; i < files.Length; i++)
+            {
+                sw.WriteLine(files[i]);
+            }
+        }
+    }
+}
---
+++ 1.0/1.0/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Cor.FileList fl = new Cor.FileList();
+            fl.Start(textBox1.Text, textBox2.Text);
+            MessageBox.Show("成功!", "提示");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog FBD = new FolderBrowserDialog();
+            FBD.Description = "选择要生成的目录";
+            if (textBox1.Text != "")
+            {
+                FBD.SelectedPath = textBox1.Text;
+
+            }
+            if (FBD.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = FBD.SelectedPath;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog SFD = new SaveFileDialog();
+            SFD.Title = "选择保存位置";
+            SFD.Filter = @"文本文件|*.txt";
+            SFD.InitialDirectory = textBox2.Text == "" ? Environment.GetFolderPath(Environment.SpecialFolder.Desktop) : textBox2.Text;
+            textBox2.Text = SFD.ShowDialog() == DialogResult.OK ? SFD.FileName : textBox2.Text;
+        }
+    }
+}
